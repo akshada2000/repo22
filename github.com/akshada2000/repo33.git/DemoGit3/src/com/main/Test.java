1e1030179e9970202cd3ef8dfea2494cc1cdaa09
HP <HP@LAPTOP-FP00JSKK.bbrouter>
2025-01-22 18:35:21 +0530

    add method add and sub by dev1

---
+++ github.com/akshada2000/repo33.git/DemoGit3/src/com/main/Test.java
@@ -0,0 +1,22 @@
+package com.main;
+
+public class Test {
+
+	//public static void main(String[] args) {
+		
+		public void addition()
+		{
+			 System.out.println("addition");
+			 
+		}
+		
+		public void subtraction()
+		{
+			System.out.println("subtraction");
+		}
+		 
+		
+
+	//}
+
+}
